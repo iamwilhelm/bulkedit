ba7e971c0faf1ae88322f40afda33a8f574ba6d2
Wil Chung <iamwil@gmail.com>
2017-03-25 21:20:40 -0700

    added csv data loading

--- main.rb
+++ main.rb
@@ -2,45 +2,52 @@
 
 require 'curses'
 require 'csv'
+require 'logger'
 
-filename = ARGV[0]
-puts filename
+CMD_WIN_H = 4
+RECIPE_WIN_W = 20
 
-#Curses.init_screen
-#Curses.noecho
-
-#begin
-#  nb_lines = Curses.lines
-#  nb_cols = Curses.cols
-#ensure
-#  Curses.close_screen
-#end
+filepath = ARGV[0]
 
 Curses.init_screen
 Curses.curs_set(0)  # Invisible cursor
-
-CMD_WIN_H = 4
+logger = Logger.new("./logs/app.log")
 
 begin
-  # Building a static window
-  cmdwin = Curses::Window.new(CMD_WIN_H, Curses.cols - 1, Curses.lines - CMD_WIN_H, 0)
+  # recipe window for list of commands
+  recipewin = Curses::Window.new(Curses.lines, RECIPE_WIN_W, 0, 0)
+  recipewin.box("|", "-")
+  recipewin.refresh
+
+  # command window to enter in commands
+  cmdwin = Curses::Window.new(CMD_WIN_H, Curses.cols - RECIPE_WIN_W, Curses.lines - CMD_WIN_H, RECIPE_WIN_W)
+  cmdwin.keypad = true    # interpret keypad
+  # cmdwin.nodelay = true  # don't wait for user input
   cmdwin.box("|", "-")
   cmdwin.setpos(1, 2)
-  cmdwin.addstr("Hello")
+  cmdwin.addstr("$ ")
   cmdwin.refresh
 
-  # In this window, there will be an animation
-  datawin = Curses::Window.new(Curses.lines - CMD_WIN_H - 1, Curses.cols - 1, 0, 0)
-  datawin.box("|", "-")
+  # In this window, there will be data
+  datawin = Curses::Window.new(Curses.lines - CMD_WIN_H, Curses.cols - RECIPE_WIN_W, 0, RECIPE_WIN_W)
   datawin.refresh
 
-  2.upto(datawin.maxx - 3) do |i|
-    datawin.setpos(datawin.maxy / 2, i)
-    datawin << "*"
-    datawin.refresh
-    sleep 0.05
+  # load the data
+  dataset = {}
+  CSV.open(filepath, headers: true) do |csv|
+    dataset[:data] = csv.read
+    dataset[:headers] = csv.headers
+  end
+  datawin.addstr(dataset[:headers].join("\t").concat("\n"))
+  datawin.addstr(dataset[:data].map { |row| row.to_a.map { |f| f[1] }.join("\t") }.join("\n"))
+  datawin.refresh
+
+  while true do
+    key = cmdwin.getch
+    cmdwin.refresh
   end
 
-rescue => ex
+rescue => err
+  logger.error [err.message, err.backtrace.join("\n")].join("\n")
   Curses.close_screen
 end
